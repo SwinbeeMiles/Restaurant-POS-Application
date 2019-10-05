a97d05fd1788b7cdd9840d86c2b6d3ba4506fc5e
jessiekaulitz <ijessie_kaulitz@yahoo.com>
2019-10-05 20:31:49 +0800

    header footer very first draft

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<html>
+<header>
+  <img src="assets/foodsmith.png" alt="cafelogo">
+</header>
+
+</html>
---
+++ includes/navArray.php
@@ -0,0 +1,25 @@
+<?php
+
+  $navItem = array(
+
+    array(
+      slug  => "homepage.php"
+      title => "Home"
+    ),
+
+    array(
+      slug  => "table.php"
+      title => "Table"
+    ),
+
+    array(
+      slug  => "menu.php"
+      title => "Menu"
+    ),
+
+    array(
+      slug  => "index.php"
+      title => "Login"
+    ),
+);
+ ?>
