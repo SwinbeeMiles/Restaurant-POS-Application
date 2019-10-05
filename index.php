59b87aeaf2045c71ecd04507d732a39c911d97cd
Zikri Dawek <maxxi_man114@hotmail.com>
2019-10-05 16:30:13 +0800

    Updated index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-    
-    
-    
+
 <head>
     <title>User Authenticator</title>
-    <meta charset = "utf-8"/>
-	
-    
-</head> 
+    <meta charset="utf-8" />
+</head>
 
 <body>
-	
+    <form>
+        Username: <input type="text" name="username"><br>
+        Password: <input type="text" name="password">
     
     
+    
+    </form>
 </body>
-</html>
 
+</html>
