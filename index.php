f895069ce195d82bef72ec2158f25cc93eea4592
Zikri Dawek <maxxi_man114@hotmail.com>
2019-10-05 16:56:33 +0800

    Updated index.php

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
 <head>
     <title>User Authenticator</title>
     <meta charset="utf-8" />
+    <script src="js/script.js"></script>
 </head>
 
 <body>
-    <form>
-        Username: <input type="text" name="username"><br>
-        Password: <input type="text" name="password">
-    
-    
-    
+    <form method="post" novalidate="novalidate">
+        <label for="username">Username:</label> <input type="text" name="username" id="_username"><br>
+        <label for="password"></label>Password: <input type="text" name="password" id="_password">
+        
+        
     </form>
+    
 </body>
 
 </html>
