757e52793c2046bdb018253019f947554892dee2
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-10-08 11:11:57 +0800

    updated db

--- database.sql
+++ database.sql
@@ -1,30 +1,30 @@
-CREATE DATABASE RestaurantDatabase;
+CREATE DATABASE restaurantdatabase;
 
-CREATE TABLE Account (
+CREATE TABLE account (
   Username varchar(20) NOT NULL,
   Password varchar(20) NOT NULL,
   AdminPrivilege boolean NOT NULL,
   PRIMARY KEY (Username)
 );
 
-INSERT INTO Account (Username, Password, AdminPrivilege) 
+INSERT INTO account (Username, Password, AdminPrivilege) 
 VALUES ('admin', 'admin', TRUE);
 
-CREATE TABLE Menu (
+CREATE TABLE menu (
 	FoodID varchar(3) NOT NULL,
 	FoodName varchar(30) NOT NULL,
 	FoodPrice double(4,2) NOT NULL,
 	PRIMARY KEY (FoodID)
 );
 
-CREATE TABLE Tables (
+CREATE TABLE tables (
 	TableID int(3) NOT NULL,
 	Chairs int(2) NOT NULL,
 	Status varchar(20) NOT NULL,
 	PRIMARY KEY (TableID)
 );
 
-CREATE TABLE Orders (
+CREATE TABLE orders (
 	OrderID int(3) NOT NULL AUTO_INCREMENT,
 	OrderDate date NOT NULL,
 	OrderTime time NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE Orders (
 	PRIMARY KEY (OrderID)
 );
 
-CREATE TABLE OrderPayment (
+CREATE TABLE orderpayment (
 	OrderID int(3) NOT NULL,
 	TotalPrice double(6,2) NOT NULL,
 	TotalPaid double(6,2) NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE OrderPayment (
 	PRIMARY KEY (OrderID)
 );
 
-CREATE TABLE OrderDetails (
+CREATE TABLE orderdetails (
 	OrderID int(3) NOT NULL,
 	FoodID varchar(3) NOT NULL,
 	Quantity int(3) NOT NULL,
