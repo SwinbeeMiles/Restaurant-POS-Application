9b713cc1e63c60a7ce9671a9dafe8c6acafaa046
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-10-08 21:57:50 +0800

    Added hidden values

--- createorder.php
+++ createorder.php
@@ -9,23 +9,26 @@
     <h1>Create Order</h1>
     <form action="createorder.php" method="get">
 		<input type="hidden" name="tableid" value="1"><br/>
-		Orange: <input type="checkbox" name="item[0][food]" value="orange" /><br/>
-		Onion: <input type="checkbox" name="item[1][food]" value="onion"/><br/>
-		Tomato: <input type="checkbox" name="item[2][food]" value="tomato"/><br/>
+		Orange Quantity: <input type="number" name="item[f05][quantity]" />
+		<input type="hidden" name="item[f05][food]" value="orange"/><br/>
+		Onion Quantity: <input type="number" name="item[f1][quantity]" />
+		<input type="hidden" name="item[f1][food]" value="onion"/><br/>
+		Tomato Quantity: <input type="number" name="item[f23][quantity]" />
+		<input type="hidden" name="item[f23][food]" value="tomato"/><br/>
 		<input type="submit" value="Order">
 	</form>
 	<?php
 		function order($i){
-			return "$i[food]";
+			return "$i[food],$i[quantity]";
 		}
 		//foodid chosen, tableid chosen
 		if(!empty($_GET['tableid']) && !empty($_GET['item'])){
 			$tableid = $_GET["tableid"];
 			$foodordered = array_map("order",$_GET['item']);
 			print_r($foodordered);
-			echo "<p>".$foodordered[0]."</p>";
-			echo "<p>".$foodordered[1]."</p>";
-			echo "<p>".$foodordered[2]."</p>";
+			echo "<p>".$foodordered['f05']."</p>";
+			echo "<p>".$foodordered['f1']."</p>";
+			echo "<p>".$foodordered['f23']."</p>";
 		}
 	?>
 </body>
