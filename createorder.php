780388fdcf7f42b84ba1eeb2ed56ed3eead56fd4
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-10-10 21:28:16 +0800

    Create Order (complete)

--- createorder.php
+++ createorder.php
@@ -9,8 +9,8 @@
     <h1>Create Order</h1>
     <form action="createorder.php" method="get">
 		<input type="hidden" name="tableid" value="1"><br/>
-		Fried Rice Quantity: <input type="number" name="item[f10][quantity]" />
-		<input type="hidden" name="item[f10][food]" value="f10"/><br/>
+		Fried Rice Quantity: <input type="number" name="item[f15][quantity]" />
+		<input type="hidden" name="item[f15][food]" value="f15"/><br/>
 		<!--Onion Quantity: <input type="number" name="item[f1][quantity]" />
 		<input type="hidden" name="item[f1][food]" value="onion"/><br/>
 		Tomato Quantity: <input type="number" name="item[f23][quantity]" />
@@ -29,63 +29,105 @@
 			$tableid = $_GET["tableid"];
 			//Get order in form of array map
 			$foodordered = array_map("order",$_GET['item']);
-			
 			//Timezone for later use
 			date_default_timezone_set("Asia/Kuala_Lumpur");
-			
+			$date = date('Y-m-d');
+			$time = date('H:i:s');
+			//Final price for later use
 			$finalprice = 0;
-			print_r($foodordered);
 			
 			//Create order instructions
+			$sql = "INSERT INTO orders (OrderDate, OrderTime, TableID) VALUES (?,?,?)";
 			$conn = connect();
-			$sql = "INSERT INTO orders (OrderDate, OrderTime, TableID) VALUES (".date('Y-m-d').",".date('H:i:s').",?)";
-			$stmt = mysqli_prepare($conn, $sql);
-			//Replace ? in sql statement
-			mysqli_stmt_bind_param($stmt, 'i', $tableid);
-			mysqli_stmt_execute($stmt);
-			
-			//Retrieve the last auto generated id
-			
-				
-			foreach($foodordered as $value){
-				//Split value taken from form
-				print_r($value);
-				$value_arr = explode(',',$value);
-				$value_id = $value_arr[0];
-				$value_quantity = $value_arr[1];
-				
-				$sql = "SELECT FoodID, FoodName, FoodPrice FROM menu WHERE FoodID = ?";
-				$stmt = mysqli_prepare($conn, $sql);
-				//Replace ? in sql statement with value_n of FoodID in array
-				mysqli_stmt_bind_param($stmt, 's', $value_id);
-				mysqli_stmt_execute($stmt);
-				//Stores new statement
-				mysqli_stmt_store_result($stmt);
-				//Bind the data to variables
-				mysqli_stmt_bind_result($stmt, $foodid, $foodname, $foodprice);
-				
-				//Sum up total price for this food and update final price for the order
-				$totalprice = $foodprice * $value_quantity;
-				$finalprice += $totalprice;
-				
-				//Populate order details based on submitted values
-				$sql = "INSERT INTO orderdetails (OrderID, FoodID, Quantity, Total) VALUES (?,?,?,?)";
-				$stmt = mysqli_prepare($conn, $sql);
+			if($stmt = mysqli_prepare($conn, $sql)){
 				//Replace ? in sql statement
-				mysqli_stmt_bind_param($stmt, 'isid', $last_id, $value_id, $value_quantity, $totalprice);
-				mysqli_stmt_execute($stmt);
+				mysqli_stmt_bind_param($stmt, 'ssi', $date, $time, $tableid);
 				
-				//Update orderpayment as pending to be paid
-				$sql = "INSERT INTO orderpayment (OrderID, TotalPrice, PaidStatus) VALUES (?,?,0)";
-				$stmt = mysqli_prepare($conn, $sql);
-				//Replace ? in sql statement
-				mysqli_stmt_bind_param($stmt, 'id', $last_id, $finalprice);
-				mysqli_stmt_execute($stmt);
-				print_r($value);
+				//Try to execute statement
+				if(mysqli_stmt_execute($stmt)){
+					//Retrieve the last auto generated id
+					$lastid = mysqli_insert_id($conn);
+					foreach($foodordered as $value){
+						//Split value taken from form
+						$value_arr = explode(',',$value);
+						$valueid = $value_arr[0];
+						$valuequantity = $value_arr[1];
+						
+						$sql = "SELECT FoodID, FoodName, FoodPrice FROM menu WHERE FoodID = ?";
+						if($stmt = mysqli_prepare($conn, $sql)){
+							//Replace ? in sql statement
+							mysqli_stmt_bind_param($stmt, 's', $valueid);
+							if(mysqli_stmt_execute($stmt)){
+								//Save the data from database to respective variables
+								mysqli_stmt_bind_result($stmt, $foodid, $foodname, $foodprice);
+								//Fetch the saved data
+								mysqli_stmt_fetch($stmt);
+								//Sum up total price for this food and update final price for the order
+								$totalprice = $foodprice * $valuequantity;
+								$finalprice += $totalprice;
+							}
+							else{
+								echo "Food details could not be retrieved.";
+							}
+						}
+						else{
+							echo "Food details statement error";
+						}
+						
+						//Close previous statement
+						mysqli_stmt_close($stmt);
+						
+						//Populate order details based on submitted values
+						$sql = "INSERT INTO `orderdetails` (`OrderID`, `FoodID`, `Quantity`, `Total`) VALUES (?,?,?,?)";
+						if($stmt = mysqli_prepare($conn, $sql)){	
+							//Replace ? in sql statement
+							mysqli_stmt_bind_param($stmt, 'isid', $lastid, $valueid, $valuequantity, $totalprice);
+							if(mysqli_stmt_execute($stmt)){
+								//Confirmation Message, delete this line if not needed
+								echo "Order details has been successfully recorded.";
+							}
+							else{
+								echo "Order details could not be inserted.";
+							}
+						}
+						else{
+							echo "Order details statement error.";
+						}
+						
+						//Close previous statement
+						mysqli_stmt_close($stmt);
+					}
+					
+					//Update orderpayment as pending to be paid
+					$sql = "INSERT INTO orderpayment (OrderID, TotalPrice, PaidStatus) VALUES (?,?,0)";
+					if($stmt = mysqli_prepare($conn, $sql)){
+						//Replace ? in sql statement
+						mysqli_stmt_bind_param($stmt, 'id', $lastid, $finalprice);
+						if(mysqli_stmt_execute($stmt)){
+							//Confirmation Message, delete this line if not needed
+							echo "Order payment has been successfully recorded.";
+						}
+						else{
+							echo "Order payment could not be inserted.";
+						}
+					}
+					else{
+						echo "Order payment statement error.";
+					}
+					
+					//Close previous statement
+					mysqli_stmt_close($stmt);
+				}
+				else{
+					echo "Data row could not be inserted into orders.";
+				}
+				
+			}
+			else{
+				echo "Orders statement error.";
 			}
 			
-			mysqli_close($conn);
-			
+			mysqli_close($conn);			
 		}
 	?>
 </body>
