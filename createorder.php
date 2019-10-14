bee9dc31009eb612a9b6b3f6ca91593c1a657e4f
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-10-14 13:01:00 +0800

    Create Order Table Update

--- createorder.php
+++ createorder.php
@@ -11,11 +11,11 @@
 		<input type="hidden" name="tableid" value="{{takenTable}}"><br/>
 		Fried Rice Quantity: <input type="number" name="item[f15][quantity]" />
 		<input type="hidden" name="item[f15][food]" value="f15"/><br/>
-		<!--Onion Quantity: <input type="number" name="item[f1][quantity]" />
-		<input type="hidden" name="item[f1][food]" value="onion"/><br/>
-		Tomato Quantity: <input type="number" name="item[f23][quantity]" />
+		Laksa Quantity: <input type="number" name="item[f14][quantity]" />
+		<input type="hidden" name="item[f14][food]" value="f14"/><br/>
+		<!--Tomato Quantity: <input type="number" name="item[f23][quantity]" />
 		<input type="hidden" name="item[f23][food]" value="tomato"/><br/> -->
-		<input type="submit" value="Order">
+		<input type="submit" value="Order" />
 	</form>
 	<?php
 		require_once 'includes/connectDB.php';
@@ -24,17 +24,23 @@
 			return "$i[food],$i[quantity]";
 		}
 		
+		//Must have table and item
 		if(!empty($_GET['tableid']) && !empty($_GET['item'])){
 			//Get tableid
 			$tableid = $_GET["tableid"];
 			//Get order in form of array map
 			$foodordered = array_map("order",$_GET['item']);
-			//Timezone for later use
+			//Timezone for later use, change timezone here
 			date_default_timezone_set("Asia/Kuala_Lumpur");
 			$date = date('Y-m-d');
 			$time = date('H:i:s');
 			//Final price for later use
 			$finalprice = 0;
+			//Success Variable
+			//Orderdetails do not take into account whether all food are recorded
+			$success_order = 0;
+			$success_orderdetails = 0;
+			$success_orderpayment = 0;
 			
 			//Create order instructions
 			$sql = "INSERT INTO orders (OrderDate, OrderTime, TableID) VALUES (?,?,?)";
@@ -47,55 +53,61 @@
 				if(mysqli_stmt_execute($stmt)){
 					//Retrieve the last auto generated id
 					$lastid = mysqli_insert_id($conn);
+					//Insert order success
+					$success_order = 1;
 					foreach($foodordered as $value){
 						//Split value taken from form
 						$value_arr = explode(',',$value);
 						$valueid = $value_arr[0];
 						$valuequantity = $value_arr[1];
 						
-						$sql = "SELECT FoodID, FoodName, FoodPrice FROM menu WHERE FoodID = ?";
-						if($stmt = mysqli_prepare($conn, $sql)){
-							//Replace ? in sql statement
-							mysqli_stmt_bind_param($stmt, 's', $valueid);
-							if(mysqli_stmt_execute($stmt)){
-								//Save the data from database to respective variables
-								mysqli_stmt_bind_result($stmt, $foodid, $foodname, $foodprice);
-								//Fetch the saved data
-								mysqli_stmt_fetch($stmt);
-								//Sum up total price for this food and update final price for the order
-								$totalprice = $foodprice * $valuequantity;
-								$finalprice += $totalprice;
+						if($valuequantity > 0){
+							$sql = "SELECT FoodID, FoodName, FoodPrice FROM menu WHERE FoodID = ?";
+							if($stmt = mysqli_prepare($conn, $sql)){
+								//Replace ? in sql statement
+								mysqli_stmt_bind_param($stmt, 's', $valueid);
+								if(mysqli_stmt_execute($stmt)){
+									//Save the data from database to respective variables
+									mysqli_stmt_bind_result($stmt, $foodid, $foodname, $foodprice);
+									//Fetch the saved data
+									mysqli_stmt_fetch($stmt);
+									//Sum up total price for this food and update final price for the order
+									$totalprice = $foodprice * $valuequantity;
+									$finalprice += $totalprice;
+								}
+								else{
+									echo "Food details could not be retrieved.";
+								}
 							}
 							else{
-								echo "Food details could not be retrieved.";
+								echo "Food details statement error";
 							}
-						}
-						else{
-							echo "Food details statement error";
-						}
-						
-						//Close previous statement
-						mysqli_stmt_close($stmt);
-						
-						//Populate order details based on submitted values
-						$sql = "INSERT INTO `orderdetails` (`OrderID`, `FoodID`, `Quantity`, `Total`) VALUES (?,?,?,?)";
-						if($stmt = mysqli_prepare($conn, $sql)){	
-							//Replace ? in sql statement
-							mysqli_stmt_bind_param($stmt, 'isid', $lastid, $valueid, $valuequantity, $totalprice);
-							if(mysqli_stmt_execute($stmt)){
-								//Confirmation Message, delete this line if not needed
-								echo "Order details has been successfully recorded.";
+							
+							//Close previous statement
+							mysqli_stmt_close($stmt);
+							
+							//Populate order details based on submitted values
+							$sql = "INSERT INTO `orderdetails` (`OrderID`, `FoodID`, `Quantity`, `Total`) VALUES (?,?,?,?)";
+							if($stmt = mysqli_prepare($conn, $sql)){	
+								//Replace ? in sql statement
+								mysqli_stmt_bind_param($stmt, 'isid', $lastid, $valueid, $valuequantity, $totalprice);
+								if(mysqli_stmt_execute($stmt)){
+									//Insert order details success
+									$success_orderdetails = 1;
+									//Confirmation Message, delete this line if not needed
+									echo "<p>Order details for ".$foodname." has been successfully recorded.</p>";
+								}
+								else{
+									echo "Order details could not be inserted.";
+								}
 							}
 							else{
-								echo "Order details could not be inserted.";
+								echo "Order details statement error.";
 							}
+							
+							//Close previous statement
+							mysqli_stmt_close($stmt);
 						}
-						else{
-							echo "Order details statement error.";
-						}
-						
-						//Close previous statement
-						mysqli_stmt_close($stmt);
 					}
 					
 					//Update orderpayment as pending to be paid
@@ -104,8 +116,10 @@
 						//Replace ? in sql statement
 						mysqli_stmt_bind_param($stmt, 'id', $lastid, $finalprice);
 						if(mysqli_stmt_execute($stmt)){
+							//Insert order payment success
+							$success_orderpayment = 1;
 							//Confirmation Message, delete this line if not needed
-							echo "Order payment has been successfully recorded.";
+							echo "<p>Order payment has been successfully recorded.</p>";
 						}
 						else{
 							echo "Order payment could not be inserted.";
@@ -125,6 +139,24 @@
 			}
 			else{
 				echo "Orders statement error.";
+			}
+			
+			if($success_order == 1 && $success_orderdetails == 1 && $success_orderpayment == 1){
+				$sql = "UPDATE tables SET Status = 'occupied' WHERE TableID = ?";
+				if($stmt = mysqli_prepare($conn, $sql)){
+					//Replace ? in sql statement
+					mysqli_stmt_bind_param($stmt, 'i', $tableid);
+					if(mysqli_stmt_execute($stmt)){
+						//Confirmation Message, delete this line if not needed
+						echo "<p>Table is updated to occupied.</p>";
+					}
+					else{
+						echo "Table could not be updated.";
+					}
+				}
+				else{
+					echo "Update tables statement error.";
+				}
 			}
 			
 			mysqli_close($conn);			
