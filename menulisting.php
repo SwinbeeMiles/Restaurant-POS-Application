899778723bb78e4831b2104d3cb8339c3876ba36
Zikri Dawek <maxxi_man114@hotmail.com>
2019-11-04 21:17:57 +0800

    Updated menulisting.php and removed redundant template

--- menulisting.php
+++ menulisting.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 
 <!-- data-ng-app="pigeon-table" in the html is essential to inject ngPigeon-table into the webpage-->
-
-<!-- This webpage is meant for non-admin usage, a normal user can view this page -->
-<html lang="en" data-ng-app="pigeon-table">
+<html lang="en">
 <head>
     <title>Menu Listing</title>
     <meta charset="utf-8"/>
-    <meta name="description" content="Modify accounts database"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+
 
     <!-- Bootstrap -->
     <link href="pigeon-table/css/bootstrap.min.css" rel="stylesheet" />
@@ -20,20 +18,21 @@
 	<!-- The includes.php file is required to include all necessary dependencies-->
     <?php
 		include "pigeon-table/php/includes.php"
-	?>
-
+	   ?>
 </head>
 
 <body>
+
   <header>
     <?php
-          include('includes/header.php');
-          include('includes/loginCheck.php');
+		      include('includes/header.php');
+		      include('includes/loginCheck.php');
+          include('includes/loginAdminCheck.php');
 
     ?>
     <div class="menuNavigation">
       <?php
-          include('includes/navMenu.php');
+		      include('includes/navMenu.php');
       ?>
     </div>
   </header>
@@ -42,22 +41,25 @@
     <div class="container">
       <div class="card cardTableBody">
         <div class="card-body cardTableBodies">
-          <button type="button" class="btn" id="adminButton"  onclick="location.href='menulist.php'">Edit Menu</button>
+          <button type="button" class="btn" id="adminButton" onclick="location.href='menulisting.php'">Back to Menu</button>
           <br><br>
-
-          <h2 class="listTitle">Menu Listing</h2>
-          <div class="container">
-            <pigeon-table query="SELECT * FROM menu" editable="false" control="false"></pigeon-table>
-          </div>
-      </div>
+        <h2 class="listTitle">Menu Listing</h2>
+        <!-- View Data in table form -->
+        <pigeon-table query="SELECT * FROM menu" editable="false" control="true"></pigeon-table>
     </div>
-  </div>
-</div>
+    </div>
+    </div>
 
+  </div>
+    <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
     <script src="pigeon-table/js/jquery.min.js"></script>
+    <!-- All Bootstrap plug-ins file -->
     <script src="pigeon-table/js/bootstrap.min.js"></script>
+    <!-- AngularJS -->
     <script src="pigeon-table/js/angular.min.js"></script>
+    <!-- Angular UI Bootstrap -->
     <script src="pigeon-table/js/ui-bootstrap-tpls-2.5.0.min.js"></script>
+    <!-- Pigeon Table -->
     <script src="pigeon-table/js/pigeon-table.js"></script>
 </body>
 </html>
