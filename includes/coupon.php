bcb54c3dce954c8864f6574dac318e5e6236ebb7
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-11-12 17:50:02 +0800

    Sleep Payment

--- includes/coupon.php
+++ includes/coupon.php
@@ -18,57 +18,57 @@
 	$date = date('Y-m-d');
 			
 		
-		$sql = "SELECT DiscountRate, ExpiryDate FROM coupons WHERE CouponCode = ?";
-		if($stmt = mysqli_prepare($conn, $sql)){
-			//Replace ? in sql statement
-			mysqli_stmt_bind_param($stmt, 's', $code);
-			if(mysqli_stmt_execute($stmt)){
-				//Stores result
-				mysqli_stmt_store_result($stmt);
-				//Check if code matches another code stored in database
-				if(mysqli_stmt_num_rows($stmt) == 1){
-					//Save the data from database to respective variables
-					mysqli_stmt_bind_result($stmt, $discount, $expirydate);
-					//Fetch the saved data
-					mysqli_stmt_fetch($stmt);
-					//Check expiry date
-					if($date > $expirydate){
-						echo "<p>Coupon Expired</p>";
-					}
-					else{
-						//Confirmation Message, delete this line if not needed
-						echo "<p>Coupon Valid</p>";
-						
-						$sql = "SELECT TotalPrice FROM orderpayment WHERE OrderID = ?";
-						if($stmt = mysqli_prepare($conn, $sql)){
-							//Replace ? in sql statement
-							mysqli_stmt_bind_param($stmt, 'i', $orderid);
-							if(mysqli_stmt_execute($stmt)){
-								//Save the data from database to respective variables
-								mysqli_stmt_bind_result($stmt, $totalprice);
-								//Fetch the saved data
-								mysqli_stmt_fetch($stmt);
-								//Calculate Balance With Coupon Code
-								$discountprice = $totalprice * (1 - $discount);
-							}
-						}
-						
-						//Close previous statement
-						mysqli_stmt_close($stmt);
-						
-						$sql = "UPDATE orderpayment SET DiscountPrice = ? WHERE OrderID = ?";
-						if($stmt = mysqli_prepare($conn, $sql)){
-							//Replace ? in sql statement
-							mysqli_stmt_bind_param($stmt, 'di', $discountprice, $orderid);
-							mysqli_stmt_execute($stmt);
-						}
-					}
+	$sql = "SELECT DiscountRate, ExpiryDate FROM coupons WHERE CouponCode = ?";
+	if($stmt = mysqli_prepare($conn, $sql)){
+		//Replace ? in sql statement
+		mysqli_stmt_bind_param($stmt, 's', $code);
+		if(mysqli_stmt_execute($stmt)){
+			//Stores result
+			mysqli_stmt_store_result($stmt);
+			//Check if code matches another code stored in database
+			if(mysqli_stmt_num_rows($stmt) == 1){
+				//Save the data from database to respective variables
+				mysqli_stmt_bind_result($stmt, $discount, $expirydate);
+				//Fetch the saved data
+				mysqli_stmt_fetch($stmt);
+				//Check expiry date
+				if($date > $expirydate){
+					echo "<p>Coupon Expired</p>";
 				}
 				else{
-					echo "<p>Invalid Coupon</p>";
+					//Confirmation Message, delete this line if not needed
+					echo "<p>Coupon Valid</p>";
+					
+					$sql = "SELECT TotalPrice FROM orderpayment WHERE OrderID = ?";
+					if($stmt = mysqli_prepare($conn, $sql)){
+						//Replace ? in sql statement
+						mysqli_stmt_bind_param($stmt, 'i', $orderid);
+						if(mysqli_stmt_execute($stmt)){
+							//Save the data from database to respective variables
+							mysqli_stmt_bind_result($stmt, $totalprice);
+							//Fetch the saved data
+							mysqli_stmt_fetch($stmt);
+							//Calculate Balance With Coupon Code
+							$discountprice = $totalprice * (1 - $discount);
+						}
+					}
+					
+					//Close previous statement
+					mysqli_stmt_close($stmt);
+					
+					$sql = "UPDATE orderpayment SET DiscountPrice = ? WHERE OrderID = ?";
+					if($stmt = mysqli_prepare($conn, $sql)){
+						//Replace ? in sql statement
+						mysqli_stmt_bind_param($stmt, 'di', $discountprice, $orderid);
+						mysqli_stmt_execute($stmt);
+					}
 				}
 			}
+			else{
+				echo "<p>Invalid Coupon</p>";
+			}
 		}
+	}
 	
 	mysqli_close($conn);
 ?>
